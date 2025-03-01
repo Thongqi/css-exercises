54e878a87facc633948f01f99edef66ee72f2498
Thongqi <141812298+Thongqi@users.noreply.github.com>
2025-03-01 15:25:44 +0800

    2

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const div = document.createElement("div")
 
     var grid = 16
-
+    let i = 0;
     while (i < grid){
         container.appendChild(div)
         i++
