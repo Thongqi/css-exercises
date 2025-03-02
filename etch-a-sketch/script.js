6f80904cdd092f1bb1457383afb6bff0cd94a06a
Thongqi <141812298+Thongqi@users.noreply.github.com>
2025-03-02 11:41:15 +0800

    add reset

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -3,13 +3,52 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const div = document.createElement("div")
 
-    var grid = 16
-    let i = 0; 
-    while (i < grid){
-        const div = document.createElement("div")
-        container.appendChild(div)
-        i++
-    }
+
+    var gridsize = getUserSize()? getUserSize:16
+    
+    createGrid(gridsize)
+
+    //change color on hover
+    var gridslists = document.querySelectorAll('.container div')
+    gridslists.forEach((item) => {
+        item.addEventListener('mouseover', () => {
+            changeGridColor(item)
+        })
+    })
+
+    //reset
+    var resetbutton = document.querySelector('#reset')
+    resetbutton.addEventListener('click', reset(gridslists))
 
     
 })
+
+function getUserSize(){
+    var usersize = document.querySelector('#grid-size')
+    usersize.addEventListener('change', (event) => {
+        return this.value
+    })
+}
+
+function createGrid(gridsize){
+    let i = 0; 
+    while (i < grid){
+        while(j < grid){
+            const div = document.createElement('div')
+            container.appendChild(div)
+            j++
+        }
+        i++
+    }
+}
+
+function changeGridColor(hovereditem){
+    // add class to hovered grid
+    hovereditem.classList.add('hovered')
+}
+
+function reset(gridslists){
+    gridslists.forEach((grid) => {
+        grid.classList.remove('hovered')
+    })
+}
